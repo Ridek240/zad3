5a9dd1555fa6381ceef8f65d751bbb69627d51fc
Ridek240 <64654063+Ridek240@users.noreply.github.com>
2021-03-24 15:15:35 +0100

    Dodaj pliki projektów.

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+
+namespace zad3.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public String BuzzFizz { get; set; }
+        public String Data { get; set; }
+        public void OnGet()
+        {
+            BuzzFizz = HttpContext.Session.GetString("BuzzFizz");
+            Data = HttpContext.Session.GetString("Data");
+        }
+    }
+
+}
